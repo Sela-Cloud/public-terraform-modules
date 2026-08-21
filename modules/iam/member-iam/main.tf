8a0373296a7f1dbf6ffdb696041dc8b3ed5dc5cf
Rahul Gorai <rahulg@sela.co.in>
2026-08-21 17:51:49 +0530

    updated IAM module

--- modules/iam/member-iam/main.tf
+++ modules/iam/member-iam/main.tf
@@ -3,7 +3,7 @@ resource "google_project_iam_member" "project_iam_member" {
   project  = var.project
   role     = each.key
   member = coalesce(
-    var.principal_set_address != null ? "principalSet://${var.principal_set_address}" : null,
+    var.principal_set_address != null ? "principal://${var.principal_set_address}" : null,
     var.service_account_address != null ? "serviceAccount:${var.service_account_address}" : null,
     var.user_email_address != null ? "user:${var.user_email_address}" : null,
     ""
