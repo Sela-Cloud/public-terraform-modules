9fb77d2d5282c53b49af9223ecbe25d80c1fdcff
Rahul Gorai <rahulg@sela.co.in>
2026-08-21 19:13:45 +0530

    updated tags

--- frontend/modules/artifact-registry/main.tf
+++ frontend/modules/artifact-registry/main.tf
@@ -16,7 +16,7 @@ locals {
 }
 
 module "artifact_registry" {
-  source   = "git::https://github.com/Sela-Cloud/public-terraform-modules//modules/artifact-registry?ref=v0.5.9"
+  source   = "git::https://github.com/Sela-Cloud/public-terraform-modules//modules/artifact-registry?ref=v0.5.10"
   for_each = local.repositories
 
   project_id                        = var.project_id
--- frontend/modules/iam-project-binding/main.tf
+++ frontend/modules/iam-project-binding/main.tf
@@ -1,5 +1,5 @@
 module "iam_project_binding" {
-  source   = "git::https://github.com/Sela-Cloud/public-terraform-modules//modules/iam/member-iam?ref=v0.5.9"
+  source   = "git::https://github.com/Sela-Cloud/public-terraform-modules//modules/iam/member-iam?ref=v0.5.10"
   for_each = var.iam_project_binding
 
   project                 = var.project_id
