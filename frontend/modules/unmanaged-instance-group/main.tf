6af7ded44f66160efe9572488b37f2d1efbd7db1
Rahul Gorai <rahulg@sela.co.in>
2026-09-08 13:28:22 +0530

    chore: upgrade all terraform modules to version 0.6.24

--- frontend/modules/unmanaged-instance-group/main.tf
+++ frontend/modules/unmanaged-instance-group/main.tf
@@ -3,7 +3,7 @@
  *****************************************/
 
 module "unmanaged_instance_group" {
-  source   = "git::https://github.com/Sela-Cloud/public-terraform-modules//modules/unmanaged-instance-group?ref=v0.6.23"
+  source   = "git::https://github.com/Sela-Cloud/public-terraform-modules//modules/unmanaged-instance-group?ref=v0.6.24"
   for_each = var.unmanaged_instance_group
 
   project_id  = var.project_id
