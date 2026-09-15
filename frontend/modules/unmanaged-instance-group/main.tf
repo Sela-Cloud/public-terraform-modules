b071d2c63726bc3efb4aefb96bd1734631e5f1f4
Gaurav Bhutada <gauravbhutada@users.noreply.github.com>
2026-09-15 09:10:58 +0000

    Updated tags

--- frontend/modules/unmanaged-instance-group/main.tf
+++ frontend/modules/unmanaged-instance-group/main.tf
@@ -3,7 +3,7 @@
  *****************************************/
 
 module "unmanaged_instance_group" {
-  source   = "git::https://github.com/Sela-Cloud/public-terraform-modules//modules/unmanaged-instance-group?ref=v0.7.0"
+  source   = "git::https://github.com/Sela-Cloud/public-terraform-modules//modules/unmanaged-instance-group?ref=v0.7.3"
   for_each = var.unmanaged_instance_group
 
   project_id  = var.project_id
